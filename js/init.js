cb44022fe50356c21af5ce2d4ba8e6fc894b72ab
serant <seran.thirugnanam@gmail.com>
2016-12-20 00:53:59 -0500

    Added initial content

--- js/init.js
+++ js/init.js
@@ -1,7 +1,21 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.profile-picture')
+    .transition({
+        animation: 'fade in',
+        duration: '1s'
+    })
 
+    $('.name-header')
+    .transition({
+        animation: 'fade in',
+        duration: '1s'
+    })
+
+    $('.education-header')
+    .transition({
+        animation: 'fade in',
+        duration: '3s'})
   }); // end of document ready
 })(jQuery); // end of jQuery name space
